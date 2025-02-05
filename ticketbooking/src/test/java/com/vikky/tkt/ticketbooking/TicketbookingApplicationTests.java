823d5259eb038f6613fa48f981b4718d5643cae4
vigneshkotha <senoritavikky@gmail.com>
2025-02-05 09:55:11 +0530

    Add files via upload

---
+++ ticketbooking/src/test/java/com/vikky/tkt/ticketbooking/TicketbookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikky.tkt.ticketbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketbookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
